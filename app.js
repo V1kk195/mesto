389b581dd679b57516cc8a92ba9bb7b13d274ddd
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-06-26 00:23:47 +0300

    server is ready

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
 
+const users = require('./routes/users');
+const cards = require('./routes/cards');
+const user = require('./routes/user');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/users', users);
+app.use('/cards', cards);
+app.use('/', user);
+app.get('*', (req, res) => {
+  res.status(404);
+  res.send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
