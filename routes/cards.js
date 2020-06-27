0b1a5f32e0195fa4221e36bf68b98bbb45fe471d
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-06-27 22:09:31 +0300

    fixed cards.js, file is read through fs.promises.readFile

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const cards = require('../data/cards.json');
+const fsPromises = require('fs').promises;
+// const cards = require('../data/cards.json');
 
 router.get('/', (req, res) => {
-  res.send(cards);
+  fsPromises.readFile('./data/cards.json', { encoding: 'utf8' })
+    .then((data) => {
+      const obj = JSON.parse(data);
+      res.type('application/json').send(obj);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 });
 
 module.exports = router;
