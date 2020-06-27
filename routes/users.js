e204ec92b7c7595b4be8db3f4f0e2d1c58eb0f49
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-06-27 13:36:13 +0300

    moved user route to users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
-const users = require('../data/users.json');
+let users = require('../data/users.json');
 
-router.get('/', (req, res) => {
+router.get('/users', (req, res) => {
   res.send(users);
+});
+
+router.get('/users/:id', (req, res) => {
+  users = JSON.parse(JSON.stringify(users).split('"_id":').join('"id":'));
+  const user = users.find((elem) => elem.id === req.params.id);
+  if (user === undefined) {
+    res.status(404);
+    res.send({ message: 'Нет пользователя с таким id' });
+  } else {
+    res.send(JSON.stringify(user));
+  }
 });
 
 module.exports = router;
