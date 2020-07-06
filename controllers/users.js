31cd590af4681d0d3e3d39ad96fa79dccc03b086
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-07-06 19:51:24 +0300

    Created controller for users

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+const Users = require('../models/users');
+
+module.exports.getUsers = (req, res) => {
+  Users.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.getUsers = (req, res) => {
+  Users.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  Users.create({ name, about, avatar });
+};
