cd56d0aeec2d4eb574118db9fb8c7ccde4ad417f
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-07-06 19:56:11 +0300

    Added then/catch to controllers/users

--- controllers/users.js
+++ controllers/users.js
@@ -6,13 +6,15 @@ module.exports.getUsers = (req, res) => {
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
-module.exports.getUsers = (req, res) => {
+module.exports.getUser = (req, res) => {
   Users.findById(req.params.id)
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка, не удалось найти юзера' }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  Users.create({ name, about, avatar });
+  Users.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка. Юзер не создан' }));
 };
