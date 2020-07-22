436099a946839ab3dccbedc2eaff261e4fc03822
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-07-22 23:34:26 +0300

    Added celebrate validation in cards router

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,34 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, createCard, deleteCard, addLike, removeLike,
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
 
-router.post('/cards', createCard);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().uri(),
+  }),
+}), createCard);
 
-router.delete('/cards/:cardId', deleteCard);
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
 
-router.put('/cards/:cardId/likes', addLike);
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), addLike);
 
-router.delete('/cards/:cardId/likes', removeLike);
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), removeLike);
 
 module.exports = router;
