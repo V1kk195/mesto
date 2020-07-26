4806f68bd158265c0e2f863294f03fb0db485131
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-07-26 23:03:26 +0300

    Refactored sending errors in auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
+const UnauthorisedError = require('../errors/unauthorized-error');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return new UnauthorisedError('Необходима авторизация');
   }
 
   const token = req.cookies.jwt;
@@ -15,9 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new UnauthorisedError('Необходима авторизация'));
   }
 
   req.user = payload;
