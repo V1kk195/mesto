73f538b7eb358bd13f1a3ca20a7d798524605958
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-07-27 21:04:35 +0300

    Fixed auth middleware and errors middleware

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,14 @@ const UnauthorisedError = require('../errors/unauthorized-error');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  if (!req.cookies.jwt) {
-    return new UnauthorisedError('Необходима авторизация');
-  }
-
   const token = req.cookies.jwt;
   let payload;
 
   try {
+    if (!req.cookies.jwt) {
+      // noinspection ExceptionCaughtLocallyJS
+      throw new UnauthorisedError('Необходима авторизация');
+    }
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorisedError('Необходима авторизация'));
